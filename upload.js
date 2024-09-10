88988a96e35ba0bb63325b76aaaaa81822e608f3
Kommidi Harshavardhan Reddy <162258368+Harsha21904@users.noreply.github.com>
2024-09-10 12:21:24 +0530

    Delete upload.js

--- upload.js
+++
@@ -1,8 +0,0 @@
-let fileupload = document.getElementsByClassName('upload-box');
-fileupload.addEventListener('input',()=>{
-    if(fileupload.files.length){
-        let filename = fileupload.files[0].name;
-        console.log(filename);
-    }
-}
-);
